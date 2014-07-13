64b3c45846e8e65dc3eac607d35aeb742532512b
Matthew Marzin <mwmarzin@gmail.com>
2014-07-12 21:39:22 -0400

    Patch Factor update
    This was missed in the last commit.

---
+++ src/irrigation/abox/PatchFactory.java
@@ -0,0 +1,46 @@
+package irrigation.abox;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class PatchFactory implements OntologyFactory
+{
+    Model model = null;
+    SoilFactory sFactory = null;
+    Patch patch = null;
+    public PatchFactory(Model model)
+    {
+        this.model = model;
+        sFactory = new SoilFactory(model);
+    }
+    
+    public Patch makePatch(SoilType soilType, CropType cropType )
+    {
+        this.patch = new Patch(model);
+        
+        setSoil(soilType, cropType);
+        
+        return this.patch;
+    }
+    
+    public Patch makePatch(SoilType soilType, CropType cropType, Location location )
+    {
+        this.patch = new Patch(model);
+        
+        setSoil(soilType, cropType);
+        setLocation(location);
+        
+        return this.patch;
+    }
+    
+    private void setLocation(Location location)
+    {
+        patch.setLocation(location);
+    }
+    
+    private void setSoil(SoilType soilType, CropType cropType)
+    {
+        Soil soil = sFactory.createSoil(soilType,cropType);
+        
+        this.patch.setSoil(soil);
+    }
+}
